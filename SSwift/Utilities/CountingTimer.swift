382b318690fcbc5075bc9c7bf7436977473305a5
sondang <son.dang@fccapital.com.au>
2018-09-26 14:44:55 +0700

    add utilities

---
+++ SSwift/Utilities/CountingTimer.swift
@@ -0,0 +1,86 @@
+//
+//  CountingTimer.swift
+//  FinstroPay
+//
+//  Created by sondang on 6/8/18.
+//  Copyright © 2018 sondang. All rights reserved.
+//
+
+import UIKit
+
+class RepeatTask: BaseObject {
+    
+    var timer: Timer!
+    var timeInterval = 1
+    var counting: ((_ countingNumber: Int) -> Void)?
+    var complete: (() -> Void)?
+    
+    // repeat infinite
+    init(withDelay timeInterval: Int?, completeblock: (() -> Void)?) {
+        super.init()
+        
+        if completeblock != nil {
+            self.complete = completeblock!
+        }
+        
+        if timeInterval != nil {
+            self.timeInterval = timeInterval!
+        }
+    }
+    
+    @objc func repeatComplete() {
+        if complete != nil {
+            complete!()
+        }
+    }
+    
+    func start() {
+        stop()
+        timer = Timer.scheduledTimer(timeInterval: TimeInterval(self.timeInterval), target: self, selector: #selector(repeatComplete), userInfo: nil, repeats: true)
+    }
+    
+    func stop() {
+        if timer != nil {
+            timer!.invalidate()
+            timer = nil
+        }
+    }
+    
+    func suppend() {
+        if timer != nil {
+            timer!.invalidate()
+        }
+    }
+    
+}
+
+class CountingTask: RepeatTask {
+    
+    var countdownSecond = 0
+    
+    init(countDown second: Int, countingBlock: ((_ countingNumber: Int) -> Void)?, completeblock: (() -> Void)?) {
+        super.init(withDelay: 1, completeblock: completeblock)
+        
+        countdownSecond = second
+        if countingBlock != nil {
+            self.counting = countingBlock!
+        }
+    }
+    
+    @objc override func repeatComplete() {
+        countdownSecond -= 1
+        if countdownSecond < 0 {
+            if complete != nil {
+                complete!()
+            }
+            
+            stop()
+        }
+        else {
+            if counting != nil {
+                counting!(countdownSecond)
+            }
+        }
+    }
+    
+}
---
+++ SSwift/Utilities/SAuthenUtility.swift
@@ -0,0 +1,66 @@
+//
+//  SAuthenUtility.swift
+//  FinstroPay
+//
+//  Created by sondang on 9/26/18.
+//  Copyright © 2018 sondang. All rights reserved.
+//
+
+import UIKit
+
+class SKeyChainUtility {
+    
+    static func save(key: String, value: String, finished: (() -> ())?) {
+        let passwordItem = KeychainPasswordItem(
+            service: KeychainConfiguration.serviceName,
+            account: key,
+            accessGroup: KeychainConfiguration.accessGroup
+        )
+        do {
+            try passwordItem.savePassword(value)
+            if finished != nil {
+                finished!()
+            }
+        } catch {
+            
+        }
+    }
+    
+    static func load(key: String, finished:((_ properties: String?) -> ())?) {
+        let passwordItem = KeychainPasswordItem(
+            service: KeychainConfiguration.serviceName,
+            account: key,
+            accessGroup: KeychainConfiguration.accessGroup
+        )
+        do {
+            let storedPassword = try passwordItem.readPassword()
+            finished?(storedPassword)
+        } catch {
+            finished?(nil)
+        }
+    }
+    
+}
+
+private struct KeychainConfiguration {
+    
+    static let serviceName = "FinstroPayServiceName"
+    
+    /*
+     Specifying an access group to use with `KeychainPasswordItem` instances
+     will create items shared accross both apps.
+     
+     For information on App ID prefixes, see:
+     https://developer.apple.com/library/ios/documentation/General/Conceptual/DevPedia-CocoaCore/AppID.html
+     and:
+     https://developer.apple.com/library/ios/technotes/tn2311/_index.html
+     */
+    //    static let accessGroup = "[YOUR APP ID PREFIX].com.example.apple-samplecode.GenericKeychainShared"
+    
+    /*
+     Not specifying an access group to use with `KeychainPasswordItem` instances
+     will create items specific to each app.
+     */
+    static let accessGroup: String? = nil
+    
+}
